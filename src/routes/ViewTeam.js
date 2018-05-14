d6b84e7d474c15d33771bcff8d6b60cb661b9538
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-05-13 22:27:47 -0500

    - channel, users, send message components;

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -4,21 +4,24 @@ import Teams from '../components/Teams';
 import Header from '../components/Header';
 import AppLayout from '../components/AppLayout';
 import Messages from '../components/Messages';
-import Input from '../components/Input';
+import SendMessage from '../components/SendMessage';
 
 export default () => (
   <AppLayout>
-    <Teams>Team</Teams>
-    <Channels>Channels</Channels>
-    <Header>Header</Header>
+    <Teams teams={[{ id: 1, letter: 'T' }, { id: 2, letter: 'Q' }]}/>
+    <Channels
+      teamName="My team"
+      username="LetItRock"
+      channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
+      users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'User1' }]}
+    />
+    <Header channelName="general" />
     <Messages>
       <ul className="message-list">
         <li></li>
         <li></li>
       </ul>
     </Messages>
-    <Input>
-      <input type="text" placeholder="CSS Grid" />
-    </Input>
+    <SendMessage channelName="general" />
   </AppLayout>
 );
