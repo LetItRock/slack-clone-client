ea4dce808b8b4df927a580a2ce53178b87430336
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-05-17 20:45:00 -0500

    - web socket configuration for apollo with subscriptions; - move apollo logic to separate file;

---
+++ src/apollo.js
@@ -0,0 +1,62 @@
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink, split } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8081/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:8081/subscriptions',
+  options: {
+    reconnect: true,
+  },
+});
+
+const authLink = setContext((_, { headers }) => {  /* eslint-disable */
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      ['x-token']: token ? token : '',
+      ['x-refresh-token']: refreshToken ? refreshToken : '',
+    }
+  }
+});
+
+const tokenAfterwareLink = new ApolloLink((operation, forward) => {
+  const { headers } = operation.getContext();
+  if (headers) {
+    const token = headers.get('x-token');
+    const refreshToken = headers.get('x-refresh-token');
+    if (token) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('refreshToken', refreshToken);
+    }
+  }
+  return forward(operation);
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  tokenAfterwareLink.concat(authLink.concat(httpLink)),
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { ApolloLink } from 'apollo-link';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import 'semantic-ui-css/semantic.min.css';
-
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:8081/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {  /* eslint-disable */
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-  const refreshToken = localStorage.getItem('refreshToken');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      ['x-token']: token ? token : '',
-      ['x-refresh-token']: refreshToken ? refreshToken : '',
-    }
-  }
-});
-
-const tokenAfterwareLink = new ApolloLink((operation, forward) => {
-  const { headers } = operation.getContext();
-  if (headers) {
-    const token = headers.get('x-token');
-    const refreshToken = headers.get('x-refresh-token');
-    if (token) {
-      localStorage.setItem('token', token);
-      localStorage.setItem('refreshToken', refreshToken);
-    }
-  }
-  return forward(operation);
-});
-
-const client = new ApolloClient({
-  link: tokenAfterwareLink.concat(authLink.concat(httpLink)),
-  cache: new InMemoryCache(),
-  connectToDevTools: true,
-});
+import client from './apollo';
+import 'semantic-ui-css/semantic.min.css';
 
 const App = (
   <ApolloProvider client={client}>
