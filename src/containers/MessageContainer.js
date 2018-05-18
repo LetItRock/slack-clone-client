27f4ffe74af6be550270f11e081efe08e097d53a
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-05-17 20:05:44 -0500

    - display channel messages list;

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Comment } from 'semantic-ui-react';
 import Messages from '../components/Messages';
 
 const MessageContainer = ({ data: { loading, messages }, channelId }) => loading ? null : (
   <Messages>
-    {JSON.stringify(messages)}
+    <Comment.Group>
+      {messages.map(message => (
+        <Comment key={`${message.id}-message`}>
+          <Comment.Content>
+            <Comment.Author as="a">
+              {message.user.username}
+            </Comment.Author>
+            <Comment.Metadata>
+              {message.created_at}
+            </Comment.Metadata>
+            <Comment.Text>
+              {message.text}
+            </Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>
+                Reply
+              </Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      ))}
+    </Comment.Group>
   </Messages>
 );
 
@@ -14,7 +36,7 @@ const messagesQuery = gql`
     messages(channelId: $channelId) {
       id
       text
-      createdAt
+      created_at
       user {
         username
       }
